e4a0d28e79c5cf2bd352cb8fdf06aec6b9a0ab09
wuqiwei <wuqiwei2002@sina.com>
2025-08-12 22:44:55 +0800

    函数,闭包

--- test_operation/main.go
+++ test_operation/main.go
@@ -2,38 +2,85 @@ package main
 
 import "fmt"
 
-func main() {
-preset:
-	for i := 0; i < 5; i++ {
-		if i == 3 {
-			break preset // 跳出标签标记的循环
-		}
-		fmt.Println("当前i的值为：", i)
-	}
-	// 	gotoPreset := false
-
-	// preset:
-	// 	a := 5
-
-	// process:
-	// 	if a > 0 {
-	// 		a--
-	// 		fmt.Println("当前a的值为：", a)
-	// 		goto process
-	// 	} else if a <= 0 {
-	// 		// elseProcess:
-	// 		if !gotoPreset {
-	// 			gotoPreset = true
-	// 			goto preset
-	// 		} else {
-	// 			goto post
-	// 		}
-	// 	}
-
-	// post:
-	//
-	//	fmt.Println("main将结束，当前a的值为：", a)
+type A struct {
+	i int
 }
+
+func (a *A) add(v int) int {
+	a.i += v
+	return a.i
+}
+
+// 声明函数变量
+var function1 func(int) int
+
+// 声明闭包
+var squart2 func(int) int = func(p int) int {
+	p *= p
+	return p
+}
+
+func main() {
+	a := A{1}
+	// // 把方法赋值给函数变量
+	function1 = a.add
+
+	// // 声明一个闭包并直接执行
+	// // 此闭包返回值是另外一个闭包（带参闭包）
+	returnFunc := func() func(int, string) (int, string) {
+		fmt.Println("this is a anonymous function")
+		return func(i int, s string) (int, string) {
+			return i, s
+		}
+	}()
+
+	// // 执行returnFunc闭包并传递参数
+	// ret1, ret2 := returnFunc(1, "test")
+	// fmt.Println("call closure function, return1 = ", ret1, "; return2 = ", ret2)
+
+	fmt.Println("a.i = ", a.i)
+	fmt.Println("after call function1, a.i = ", function1(1))
+	fmt.Println("a.i = ", a.i)
+	fmt.Println("after call function1, a.i = ", a.add(2))
+
+	// fmt.Println("a.i = ", a.i)
+	// 调用通过IIFE获得的内层函数
+	ret1, ret2 := returnFunc(100, "hello")
+	fmt.Println(ret1, ret2) // 输出：100 hello
+}
+
+// func main() {
+// preset:
+// 	for i := 0; i < 5; i++ {
+// 		if i == 3 {
+// 			break preset // 跳出标签标记的循环
+// 		}
+// 		fmt.Println("当前i的值为：", i)
+// 	}
+// 	gotoPreset := false
+
+// preset:
+// 	a := 5
+
+// process:
+// 	if a > 0 {
+// 		a--
+// 		fmt.Println("当前a的值为：", a)
+// 		goto process
+// 	} else if a <= 0 {
+// 		// elseProcess:
+// 		if !gotoPreset {
+// 			gotoPreset = true
+// 			goto preset
+// 		} else {
+// 			goto post
+// 		}
+// 	}
+
+// post:
+//
+//	fmt.Println("main将结束，当前a的值为：", a)
+// }
 
 // func main() {
 
