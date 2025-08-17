930329b59376fa43557b979bef8d8014ffa41496
wuqiwei <wuqiwei2002@sina.com>
2025-08-17 23:20:43 +0800

    go

--- test_operation/main.go
+++ test_operation/main.go
@@ -1,57 +1,339 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// 只接收channel的函数
+func receiveOnly(ch <-chan int) {
+	for v := range ch {
+		fmt.Printf("接收到: %d\n", v)
+	}
+}
+
+// 只发送channel的函数
+func sendOnly(ch chan<- int) {
+	for i := 0; i < 5; i++ {
+		ch <- i //将变量 i 的值发送到通道 ch 中
+		fmt.Printf("发送: %d\n", i)
+	}
+	close(ch)
+}
 
 func main() {
-	m := make(map[string]int)
-	// m := make(map[string]int, 10)
+	// 创建一个带缓冲的channel
+	ch := make(chan int, 3)
 
-	m["1"] = int(1)
-	m["2"] = int(2)
-	m["3"] = int(3)
-	m["4"] = int(4)
-	m["5"] = int(5)
-	m["6"] = int(6)
+	// 启动发送goroutine
+	go sendOnly(ch)
 
-	// 获取元素
-	value1 := m["1"]
-	fmt.Println("m[\"1\"] =", value1)
+	// 启动接收goroutine
+	go receiveOnly(ch)
 
-	value1, exist := m["1"]
-	fmt.Println("m[\"1\"] =", value1, ", exist =", exist)
-
-	valueUnexist, exist := m["10"]
-	fmt.Println("m[\"10\"] =", valueUnexist, ", exist =", exist)
-
-	// 修改值
-	fmt.Println("before modify, m[\"2\"] =", m["2"])
-	m["2"] = 20
-	fmt.Println("after modify, m[\"2\"] =", m["2"])
-
-	// 获取map的长度
-	fmt.Println("before add, len(m) =", len(m))
-	m["10"] = 10
-	fmt.Println("after add, len(m) =", len(m))
-
-	// 遍历map集合main
-	for key, value := range m {
-		fmt.Println("iterate map, m[", key, "] =", value)
+	// 使用select进行多路复用
+	timeout := time.After(2 * time.Second)
+	for {
+		select {
+		case v, ok := <-ch:
+			if !ok {
+				fmt.Println("Channel已关闭")
+				return
+			}
+			fmt.Printf("主goroutine接收到: %d\n", v)
+		case <-timeout:
+			fmt.Println("操作超时")
+			return
+		default:
+			fmt.Println("没有数据，等待中...")
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
-
-	// 使用内置函数删除指定的key
-	_, exist_10 := m["10"]
-	fmt.Println("before delete, exist 10: ", exist_10)
-	delete(m, "10")
-	_, exist_10 = m["10"]
-	fmt.Println("after delete, exist 10: ", exist_10)
-
-	// 在遍历时，删除map中的key
-	for key := range m {
-		fmt.Println("iterate map, will delete key:", key)
-		delete(m, key)
-	}
-	fmt.Println("m = ", m)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// // 线程安全的计数器
+// type SafeCounter struct {
+// 	mu    sync.Mutex
+// 	count int
+// }
+
+// // 增加计数
+// func (c *SafeCounter) Increment() {
+// 	c.mu.Lock()
+// 	defer c.mu.Unlock()
+// 	c.count++
+// }
+
+// // 获取当前计数
+// func (c *SafeCounter) GetCount() int {
+// 	c.mu.Lock()
+// 	defer c.mu.Unlock()
+// 	return c.count
+// }
+
+// type UnsafeCounter struct {
+// 	count int
+// }
+
+// // 增加计数
+// func (c *UnsafeCounter) Increment() {
+// 	c.count += 1
+// }
+
+// // 获取当前计数
+// func (c *UnsafeCounter) GetCount() int {
+// 	return c.count
+// }
+
+// func main() {
+// 	counter := UnsafeCounter{}
+
+// 	// 启动100个goroutine同时增加计数
+// 	for i := 0; i < 1000; i++ {
+// 		go func() {
+// 			for j := 0; j < 100; j++ {
+// 				counter.Increment()
+// 			}
+// 		}()
+// 	}
+
+// 	// 等待一段时间确保所有goroutine完成
+// 	time.Sleep(time.Second)
+
+// 	// 输出最终计数
+// 	fmt.Printf("Final count: %d\n", counter.GetCount())
+// }
+
+// package main
+
+// import "fmt"
+
+// // PaymentMethod 接口定义了支付方法的基本操作
+// type PayMethod interface {
+// 	Account
+// 	Pay(amount int) bool
+// }
+
+// type Account interface {
+// 	GetBalance() int
+// }
+
+// // CreditCard 结构体实现 PaymentMethod 接口
+// type CreditCard struct {
+// 	balance int
+// 	limit   int
+// }
+
+// func (c *CreditCard) Pay(amount int) bool {
+// 	if c.balance+amount <= c.limit {
+// 		c.balance += amount
+// 		fmt.Printf("信用卡支付成功: %d\n", amount)
+// 		return true
+// 	}
+// 	fmt.Println("信用卡支付失败: 超出额度")
+// 	return false
+// }
+
+// func (c *CreditCard) GetBalance() int {
+// 	return c.balance
+// }
+
+// // DebitCard 结构体实现 PaymentMethod 接口
+// type DebitCard struct {
+// 	balance int
+// }
+
+// func (d *DebitCard) Pay(amount int) bool {
+// 	if d.balance >= amount {
+// 		d.balance -= amount
+// 		fmt.Printf("借记卡支付成功: %d\n", amount)
+// 		return true
+// 	}
+// 	fmt.Println("借记卡支付失败: 余额不足")
+// 	return false
+// }
+
+// func (d *DebitCard) GetBalance() int {
+// 	return d.balance
+// }
+
+// // 使用 PaymentMethod 接口的函数
+// func purchaseItem(p PayMethod, price int) {
+// 	if p.Pay(price) {
+// 		fmt.Printf("购买成功，剩余余额: %d\n", p.GetBalance())
+// 	} else {
+// 		fmt.Println("购买失败")
+// 	}
+// }
+
+// func main() {
+// 	// creditCard := &CreditCard{balance: 0, limit: 1000}
+// 	debitCard := &DebitCard{balance: 500}
+
+// 	// fmt.Println("使用信用卡购买:")
+// 	// purchaseItem(creditCard, 800)
+
+// 	fmt.Println("\n使用借记卡购买:")
+// 	purchaseItem(debitCard, 300)
+
+// 	// fmt.Println("\n再次使用借记卡购买:")
+// 	// purchaseItem(debitCard, 300)
+
+// 	// fmt.Println("\n再次使用信用卡购买:")
+// 	// purchaseItem(creditCard, 800)
+
+// 	var accountA Account = debitCard
+// 	fmt.Println("余额为", accountA.GetBalance())
+
+// }
+
+//把一个接口类型转换成具体的结构体接口类型。
+
+// type Supplier interface {
+// 	Get() string
+// }
+
+// type DigitSupplier struct {
+// 	value int
+// }
+
+// func (i *DigitSupplier) Get() string {
+// 	return fmt.Sprintf("%d", i.value)
+// }
+
+// func main() {
+// 	var a Supplier = &DigitSupplier{value: 1}
+// 	fmt.Println(a.Get())
+
+// 	b, ok := (a).(*DigitSupplier)
+// 	fmt.Println(b, ok)
+// }
+
+// 转换
+// func main() {
+// 	str := "123"
+// 	num, err := strconv.Atoi(str)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Printf("字符串转换为int: %d \n", num)
+// 	str1 := strconv.Itoa(num)
+// 	fmt.Printf("int转换为字符串: %s \n", str1)
+
+// 	ui64, err := strconv.ParseUint(str, 10, 32)
+// 	fmt.Printf("字符串转换为uint64: %d \n", num)
+
+// 	str2 := strconv.FormatUint(ui64, 2)
+// 	fmt.Printf("uint64转换为字符串: %s \n", str2)
+// }
+
+//类型转换
+// func main() {
+// 	var i int32 = 17
+// 	var b byte = 5
+// 	var f float32
+
+// 	// 数字类型可以直接强转
+// 	f = float32(i) / float32(b)
+// 	fmt.Printf("f 的值为: %f\n", f)
+
+// 	// 当int32类型强转成byte时，高位被直接舍弃
+// 	var i2 int32 = 256
+// 	var b2 byte = byte(i2)
+// 	fmt.Printf("b2 的值为: %d\n", b2)
+// }
+
+// range 遍历
+// func main() {
+// 	hash := map[string]int{
+// 		"a": 1,
+// 		"f": 2,
+// 		"z": 3,
+// 		"c": 4,
+// 	}
+
+// 	// for key, value := range hash {
+// 	// 	fmt.Printf("key=%s, value=%d\n", key, value)
+// 	// }
+
+// 	// for key := range hash {
+// 	// 	fmt.Printf("key=%s, value=%d\n", key, hash[key])
+// 	// }
+
+// 	for key, value := range hash {
+// 		fmt.Println("key =", key, ", value =", value)
+// 		// fmt.Println("key =", key, ", value =", value)
+// 		fmt.Println("----------------")
+
+// 	}
+// 	// for key := range hash {
+// 	// 	fmt.Printf("key=%s, value=%d\n", key, hash[key])
+// 	// }
+
+// 	// for key, value := range hash {
+// 	// 	fmt.Printf("key=%s, value=%d\n", key, value)
+// 	// }
+// }
+
+// map集合
+// func main() {
+// 	m := make(map[string]int)
+// 	// m := make(map[string]int, 10)
+
+// 	m["1"] = int(1)
+// 	m["2"] = int(2)
+// 	m["3"] = int(3)
+// 	m["4"] = int(4)
+// 	m["5"] = int(5)
+// 	m["6"] = int(6)
+
+// 	// 获取元素
+// 	value1 := m["1"]
+// 	fmt.Println("m[\"1\"] =", value1)
+
+// 	value1, exist := m["1"]
+// 	fmt.Println("m[\"1\"] =", value1, ", exist =", exist)
+
+// 	valueUnexist, exist := m["10"]
+// 	fmt.Println("m[\"10\"] =", valueUnexist, ", exist =", exist)
+
+// 	// 修改值
+// 	fmt.Println("before modify, m[\"2\"] =", m["2"])
+// 	m["2"] = 20
+// 	fmt.Println("after modify, m[\"2\"] =", m["2"])
+
+// 	// 获取map的长度
+// 	fmt.Println("before add, len(m) =", len(m))
+// 	m["10"] = 10
+// 	fmt.Println("after add, len(m) =", len(m))
+
+// 	// 遍历map集合main
+// 	for key, value := range m {
+// 		fmt.Println("iterate map, m[", key, "] =", value)
+// 	}
+
+// 	// 使用内置函数删除指定的key
+// 	_, exist_10 := m["10"]
+// 	fmt.Println("before delete, exist 10: ", exist_10)
+// 	delete(m, "10")
+// 	_, exist_10 = m["10"]
+// 	fmt.Println("after delete, exist 10: ", exist_10)
+
+// 	// 在遍历时，删除map中的key
+// 	for key := range m {
+// 		fmt.Println("iterate map, will delete key:", key)
+// 		delete(m, key)
+// 	}
+// 	fmt.Println("m = ", m)
+// }
 
 // map
 // func main() {
